23207cb6e225f51c600756edff8ca86dad1e8509
Grazyna Butrykowska <gagsster@gmail.com>
2019-03-18 00:41:45 +0100

    Added space to a file just to check how git works.

--- Zadanie1.py
+++ Zadanie1.py
@@ -1,4 +1,4 @@
-# 5. Napisz program, który wczytuje ze standardowego wejścia nieujemną liczbę całkowitą n
+# 5.  Napisz program, który wczytuje ze standardowego wejścia nieujemną liczbę całkowitą n
 # i wypisuje na standardowym wyjściu sumę kwadratów liczb od 0 do n, czyli wartość 0^2 + 1^2 + 3^2 + ... + n^2.
 
 n = int(input("Type in the number: "))
