2fcbe1419ef29957f81118c297b2c2925dc3cc59
Grazyna Butrykowska <gagsster@gmail.com>
2019-07-26 16:53:07 +0200

    Minor corrections after review.

--- Zadanie6.py
+++ Zadanie6.py
@@ -1,25 +1,16 @@
 class Calculator(object):
 
-    def __init__(self, number1, number2):
-        self.number1 = number1
-        self.number2 = number2
-
     def add(number1, number2):
-        sum = float(number1) + float(number2)
-        return sum
+        return float(number1) + float(number2)
 
     def substract(number1, number2):
-        substraction = float(number1) - float(number2)
-        return substraction
+        return float(number1) - float(number2)
 
     def multiply(number1, number2):
-        multiplication = float(number1) * float(number2)
-        return multiplication
+        return float(number1) * float(number2)
 
     def divide(number1, number2):
-        division = float(number1) / float(number2)
-        return division
-
+        return float(number1) / float(number2)
 
 number1 = input("Please type in first number: ")
 number2 = input("Please type in second number: ")
