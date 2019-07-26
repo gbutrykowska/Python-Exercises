31bc2d70d249f9265968b5d83db725dc09a0296b
Grazyna Butrykowska <gagsster@gmail.com>
2019-07-26 16:16:32 +0200

    Updated exercise 8 and 8.1

--- Zadanie8.1.py
+++ Zadanie8.1.py
@@ -7,40 +7,42 @@
 # zzzz*s*
 # www*www****z
 # *zd*qm*wp*bs*
-
-contest_string = input("Please type in the contest string: ")
-contest_list = list(contest_string)
-
-def letterbattle(contest_list):
-
+def get_contest_list_after_bombs(contest_list):
     if "*" in contest_list:
         position = contest_list.index("*")
-        if position > len(contest_list):
+        if position < len(contest_list) - 1:
             del contest_list[position - 1]
             del contest_list[position + 1]
         else:
             del contest_list[position - 1]
-
+    return contest_list
+def get_left_score(contest_list):
     left_score = 0
-    right_score = 0
-    for i in contest_list:
-        if i == "w":
+    for letter in contest_list:
+        if letter == "w":
             left_score += 4
-        if i == "p":
+        if letter == "p":
             left_score += 3
-        if i == "b":
+        if letter == "b":
             left_score += 2
-        if i == "s":
+        if letter == "s":
             left_score += 1
-        if i == "m":
-            right_score += 4
-        if i == "q":
-            right_score += 3
-        if i == "d":
-            right_score += 2
-        if i == "z":
-            right_score += 1
+    return left_score
 
+def get_right_score(contest_list):
+    right_score = 0
+    for letter in contest_list:
+        if letter == "m":
+            right_score += 4
+        if letter == "q":
+            right_score += 3
+        if letter == "d":
+            right_score += 2
+        if letter == "z":
+            right_score += 1
+    return right_score
+
+def print_letter_battle_result(left_score, right_score):
     if left_score == right_score:
         print(" It's a draw! ")
     elif left_score > right_score:
@@ -48,4 +50,12 @@ def letterbattle(contest_list):
     else:
         print(" Right side won!")
 
-letterbattle(contest_list)
+
+
+
+contest_string = input("Please type in the contest string: ")
+contest_list = list(contest_string)
+contest_list = get_contest_list_after_bombs(contest_list)
+left_score = get_left_score(contest_list)
+right_score = get_right_score(contest_list)
+print_letter_battle_result(left_score, right_score)
--- Zadanie8.py
+++ Zadanie8.py
@@ -1,43 +1,46 @@
-# 8. Istnieją dwie grupy walczących liter. Napisz program, który wczytuje
-# String rozgrywki składający się z liter oraz decyduje, która grupa zwyciężyła turę.
-# Lewa strona:
-# w - 4
-# p - 3
-# b - 2
-# s - 1
-# Prawa strona:
-# m - 4
-# q - 3
-# d - 2
-# z - 1
-# Pozostałe litery nie mają znaczenia w rozgrywce.
-# Przykład: "zdqmwpbs"
-# Przydatna będzie metoda: String.toCharArray
+#8. Istnieją dwie grupy walczących liter. Napisz program, który wczytuje String rozgrywki
+# składający się z liter oraz decyduje, która grupa zwyciężyła turę.
+#Lewa strona:
+#w - 4
+#p - 3
+#b - 2
+#s - 1
+#Prawa strona:
+#m - 4
+#q - 3
+#d - 2
+#z - 1
+#Pozostałe litery nie mają znaczenia w rozgrywce.
+#Przykład: "zdqmwpbs"
+#Przydatna będzie metoda: String.toCharArray
 
-contest_string = input("Please type in the contest string: ")
-contest_list = list(contest_string)
-
-def letterbattle(contest_list):
+def get_left_score(contest_list):
     left_score = 0
-    right_score = 0
-    for i in contest_list:
-        if i == "w":
+    for letter in contest_list:
+        if letter == "w":
             left_score += 4
-        if i == "p":
+        if letter == "p":
             left_score += 3
-        if i == "b":
+        if letter == "b":
             left_score += 2
-        if i == "s":
+        if letter == "s":
             left_score += 1
-        if i == "m":
-            right_score += 4
-        if i == "q":
-            right_score += 3
-        if i == "d":
-            right_score += 2
-        if i == "z":
-            right_score += 1
+    return left_score
 
+def get_right_score(contest_list):
+    right_score = 0
+    for letter in contest_list:
+        if letter == "m":
+            right_score += 4
+        if letter == "q":
+            right_score += 3
+        if letter == "d":
+            right_score += 2
+        if letter == "z":
+            right_score += 1
+    return right_score
+
+def print_letter_battle_result(left_score, right_score):
     if left_score == right_score:
         print(" It's a draw! ")
     elif left_score > right_score:
@@ -45,14 +48,12 @@ def letterbattle(contest_list):
     else:
         print(" Right side won!")
 
-letterbattle(contest_list)
 
-# 8.1 W Stringu mogą się znaleźć również bomby - znaki '*',
-# które wymazują przyległe litery, na przykład:
-# aa*aa => a__a
-# aa => ____
 
-# Przykład stringów:
-# zzzz*s*
-# www*www****z
-# *zd*qm*wp*bs*
+
+contest_string = input("Please type in the contest string: ")
+contest_list = list(contest_string)
+
+left_score = get_left_score(contest_list)
+right_score = get_right_score(contest_list)
+print_letter_battle_result(left_score, right_score)
